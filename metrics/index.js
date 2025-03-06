e0e6f95d9d6ee0c28b3df01c41bd1911dd78e95a
AndrewBlur <143100343+FakeandrewA@users.noreply.github.com>
2025-03-06 10:30:32 +0530

    Update and rename t.js to index.js

---
+++ metrics/index.js
@@ -0,0 +1,4 @@
+// src/metrics/index.js
+export { calculateWaitingTime } from './waiting-time.js';
+export { calculateTurnaroundTime } from './turnaround-time.js';
+export { calculateThroughput } from './throughput.js';
